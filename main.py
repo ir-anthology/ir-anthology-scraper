9f248a71c55fb6fa2edf1df3c8c55600b74cc4e8
Wolfgang Kircheis <wolfgang.kircheis@uni-leipzig.de>
2023-10-10 17:52:48 +0200

    Add bibtex_dump handling

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ from argparse import ArgumentParser
 from scripts.dblp_scraper import DBLPscraper
 from os.path import dirname, exists, sep
 from os import makedirs
-from json import load
+from json import dumps, loads, load
 from tqdm import tqdm
 import traceback
 
@@ -15,8 +15,14 @@ if __name__ == "__main__":
 
 
     with open("config.json") as file:
-        config = load(file)
+        config = {"cikm":[1992]}#load(file)
 
+    bibtex_dump = {}
+    if exists("bibtex_dump.txt"):
+        with open("bibtex_dump.txt") as file:
+            for line in file:
+                url, bibtex = loads(line)
+                bibtex_dump[url] = bibtex
         
     scraper = DBLPscraper("output")
 
@@ -29,7 +35,13 @@ if __name__ == "__main__":
                 print("Scraping bibtex entries...")
                 bibtex_list = []
                 for entry in tqdm(entry_list, total=len(entry_list)):
-                    bibtex_list.append(scraper.scrape_bibtex(entry))
+                    try:
+                        bibtex_list.append(bibtex_dump[entry["info"]["url"]])
+                    except KeyError:
+                        bibtex = scraper.scrape_bibtex(entry)
+                        with open("bibtex_dump.txt", "a") as file:
+                            file.write(dumps([entry["info"]["url"],bibtex]) + "\n")
+                        bibtex_list.append(bibtex)
                 bibtex_string = scraper.generate_bibtex_string(entry_list, bibtex_list)
 
                 write_bibtex_string_to_file(bibtex_string, scraper.output_directory + sep + conference + "-" + str(year) + ".txt")
