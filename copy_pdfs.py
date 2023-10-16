4d4b0d4b65ff891c8e760feb1614b1aa7da27fd8
Wolfgang Kircheis <wolfgang.kircheis@uni-leipzig.de>
2023-10-16 18:21:48 +0200

    Update

--- copy_pdfs.py
+++ copy_pdfs.py
@@ -7,8 +7,7 @@ from os.path import dirname, exists, sep
 from os import makedirs
 from tqdm import tqdm
 
-bibfilepaths = glob("/media/" + getuser() + "/Ceph/data-in-production/ir-anthology/pdfs/*/*/*.bib")
-pdffilepaths = glob("/media/" + getuser() + "/Ceph/data-in-production/ir-anthology/sources/wlgc/papers-by-doi/*/*")
+bibfilepaths = sorted(glob("/media/" + getuser() + "/Ceph/data-in-production/ir-anthology/pdfs/civr/*/*.bib"))
 log_file_path = "/media/" + getuser() + "/Ceph/data-in-progress/data-research/ir-anthology-scraper/output/copy_pdfs_log.txt"
 entry_count = 0
 pdf_count = 0
