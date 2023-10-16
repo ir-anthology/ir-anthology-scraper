f15fe7d997fef9a806e34c53e68af3455b81f6d1
Wolfgang Kircheis <wolfgang.kircheis@uni-leipzig.de>
2023-10-16 17:02:31 +0200

    Add pdf copy script

---
+++ copy.py
@@ -0,0 +1,23 @@
+import bibtexparser
+from getpass import getuser
+from glob import glob
+from os.path import exists
+from tqdm import tqdm
+
+bibfilepaths = glob("/media/" + getuser() + "/Data/Work/github/ir-anthology-scraper/output/pdfs/*/*/*.bib")
+pdffilepaths = glob("/media/" + getuser() + "/Ceph/data-in-production/ir-anthology/sources/wlgc/papers-by-doi/*/*")
+
+entry_count = 0
+pdf_count = 0
+
+for bibfilepath in tqdm(bibfilepaths, total=len(bibfilepaths)):
+    with open(bibfilepath) as bibfile:
+        entries = bibtexparser.load(bibfile).entries
+        for entry in entries:
+            entry_count += 1
+            if "doi" in entry:
+                if exists("/media/wolfgang/Ceph/data-in-production/ir-anthology/sources/wlgc/papers-by-doi/" + entry["doi"] + ".pdf"):
+                    pdf_count += 1
+
+print(pdf_count)
+print(entry_count)
