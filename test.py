34ba7cdc609f470b59645c3fa1e3c1fc34e4abcb
Wolfgang Kircheis <wolfgang.kircheis@uni-leipzig.de>
2023-10-16 17:28:06 +0200

    Update test.py

--- test.py
+++ test.py
@@ -17,6 +17,9 @@ for bibfilepath in tqdm(bibfilepaths, total=len(bibfilepaths)):
             entry_count += 1
             if "doi" in entry:
                 if exists("/media/" + getuser() + "/Ceph/data-in-production/ir-anthology/sources/wlgc/papers-by-doi/" + entry["doi"] + ".pdf"):
+                    print(bibfilepath)
+                    print("/media/" + getuser() + "/Ceph/data-in-production/ir-anthology/sources/wlgc/papers-by-doi/" + entry["doi"] + ".pdf")
+                    input()
                     pdf_count += 1
 
 print(pdf_count)
