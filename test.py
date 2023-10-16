1ad33dea0e98c3034912569efbc45a6e364f0487
Wolfgang Kircheis <wolfgang.kircheis@uni-leipzig.de>
2023-10-16 18:08:21 +0200

    test.py

--- test.py
+++ test.py
@@ -6,22 +6,28 @@ from os.path import dirname, exists, sep
 from os import makedirs
 from tqdm import tqdm
 
-bibfilepaths = glob("/media/" + getuser() + "/Ceph/data-in-production/ir-anthology/pdfs/*/*/*.bib")
+bibfilepaths = glob("/media/" + getuser() + "/Ceph/data-in-production/ir-anthology/pdfs/civr/*/*.bib")
 pdffilepaths = glob("/media/" + getuser() + "/Ceph/data-in-production/ir-anthology/sources/wlgc/papers-by-doi/*/*")
 
 entry_count = 0
 pdf_count = 0
 
-for bibfilepath in tqdm(bibfilepaths, total=len(bibfilepaths)):
+for bibfilepath_count, bibfilepath in enumerate(bibfilepaths, 1):
     with open(bibfilepath) as bibfile:
+        with open("log.txt", "a") as logfile:
+            logfile.write("/".join(bibfilepath.split("/")[-3:-1]) + "\n")
         entries = bibtexparser.load(bibfile).entries
         for entry in entries:
             entry_count += 1
             if "doi" in entry:
                 pdf_src_path = "/media/" + getuser() + "/Ceph/data-in-production/ir-anthology/sources/wlgc/papers-by-doi/" + entry["doi"] + ".pdf"
                 if exists(pdf_src_path):
-                    copyfile(pdf_src_path, dirname(bibfilepath) + sep + entry["ID"] + ".pdf")
+                    pdf_dst_path = dirname(bibfilepath) + sep + entry["ID"] + ".pdf"
+                    copyfile(pdf_src_path, pdf_dst_path)
                     pdf_count += 1
-
-print(pdf_count)
-print(entry_count)
+                    with open("log.txt", "a") as logfile:
+                        logfile.write("   - " + entry["ID"] + ".pdf" + "\n")
+                        
+with open("log.txt", "a") as logfile:
+    logfile.write("PDFs: " + str(pdf_count) + "\n")
+    logfile.write("PDFs: " + str(entry_count) + "\n")
