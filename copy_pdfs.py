17270b1801861261062dbf67c43f44adecdaaa52
Wolfgang Kircheis <wolfgang.kircheis@uni-leipzig.de>
2023-10-17 13:51:24 +0200

    Update copy_pdfs.py

--- copy_pdfs.py
+++ copy_pdfs.py
@@ -5,15 +5,17 @@ from shutil import copyfile
 from glob import glob
 from os.path import dirname, exists, sep
 from os import makedirs
-from tqdm import tqdm
 
-bibfilepaths = sorted(glob("/media/" + getuser() + "/Ceph/data-in-production/ir-anthology/pdfs/*/*/*.bib"))
-log_file_path = "/media/" + getuser() + "/Ceph/data-in-progress/data-research/ir-anthology-scraper/output/copy_pdfs_log.txt"
+ir_anthology_root = "../" #"/media/" + getuser() + "/Ceph/data-in-production/ir-anthology/"
+
+bibfilepaths = sorted(glob(ir_anthology_root + "conferences/*/*/*.bib"))
+log_file_path = "copy_pdfs_log.txt"
 entry_count = 0
 pdf_count = 0
 
-now = datetime.now()
+DRY_RUN = True
 
+now = datetime.now()
 with open(log_file_path, "a") as logfile:
     logfile.write(str(now.year) + "-" + str(now.month) + "-" + (str(now.day).rjust(2,"0")) + " " +
                   str(now.hour) + ":" + str(now.minute) + ":" + (str(now.second).rjust(2,"0")) + "\n\n")
@@ -25,14 +27,19 @@ for bibfilepath_count, bibfilepath in enumerate(bibfilepaths, 1):
         entries = bibtexparser.load(bibfile).entries
         for entry in entries:
             entry_count += 1
-            if "doi" in entry:
-                pdf_src_path = "/media/" + getuser() + "/Ceph/data-in-production/ir-anthology/sources/wlgc/papers-by-doi/" + entry["doi"] + ".pdf"
-                if exists(pdf_src_path):
-                    pdf_dst_path = dirname(bibfilepath) + sep + entry["ID"] + ".pdf"
-                    copyfile(pdf_src_path, pdf_dst_path)
-                    pdf_count += 1
-                    with open(log_file_path, "a") as logfile:
-                        logfile.write("   - " + entry["ID"] + ".pdf" + "\n")
+            pdf_dst_path = dirname(bibfilepath) + sep + entry["ID"] + ".pdf"
+            if exists(pdf_dst_path):
+                pdf_count += 1
+                with open(log_file_path, "a") as logfile:
+                    logfile.write("   - " + entry["ID"] + ".pdf" + "\n")
+            else:
+                if "doi" in entry:
+                    pdf_src_path = ir_anthology_root + "sources/wlgc/papers-by-doi/" + entry["doi"] + ".pdf"
+                    if exists(pdf_src_path):
+                        if not DRY_RUN: copyfile(pdf_src_path, pdf_dst_path)
+                        pdf_count += 1
+                        with open(log_file_path, "a") as logfile:
+                            logfile.write("   - " + entry["ID"] + ".pdf" + "\n")
                         
 with open(log_file_path, "a") as logfile:
     logfile.write("\nPDFs: " + str(pdf_count) + "\n")
