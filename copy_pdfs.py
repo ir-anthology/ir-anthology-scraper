776adfee54efc03640dc94770ead137efb7d7c2a
Wolfgang Kircheis <wolfgang.kircheis@uni-leipzig.de>
2023-10-17 17:47:30 +0200

    Update

--- copy_pdfs.py
+++ copy_pdfs.py
@@ -4,43 +4,66 @@ from datetime import datetime
 from shutil import copyfile
 from glob import glob
 from os.path import dirname, exists, sep
+from csv import writer
 from os import makedirs
+from tqdm import tqdm
 
 ir_anthology_root = "../" #"/media/" + getuser() + "/Ceph/data-in-production/ir-anthology/"
 
+now = datetime.now()
+start = (str(now.year) + "-" + str(now.month) + "-" + (str(now.day).rjust(2,"0")) + "_" +
+         str(now.hour) + "-" + str(now.minute) + "-" + (str(now.second).rjust(2,"0")))
+
+log_file_path = "copy_pdfs_" + start + ".csv"
+
 bibfilepaths = sorted(glob(ir_anthology_root + "conferences/*/*/*.bib"))
-log_file_path = "copy_pdfs_log.txt"
+
 entry_count = 0
 pdf_count = 0
 
 DRY_RUN = True
 
-now = datetime.now()
-with open(log_file_path, "a") as logfile:
-    logfile.write(str(now.year) + "-" + str(now.month) + "-" + (str(now.day).rjust(2,"0")) + " " +
-                  str(now.hour) + ":" + str(now.minute) + ":" + (str(now.second).rjust(2,"0")) + "\n\n")
+# CSV FORMAT: NUMBER, VENUE, YEAR, AUTHOR, DOI, WLGC, WCSP15
 
-for bibfilepath_count, bibfilepath in enumerate(bibfilepaths, 1):
+for bibfilepath in tqdm(bibfilepaths, total=len(bibfilepaths)):
     with open(bibfilepath) as bibfile:
-        with open(log_file_path, "a") as logfile:
-            logfile.write("/".join(bibfilepath.split("/")[-3:-1]) + "\n")
+            
         entries = bibtexparser.load(bibfile).entries
+        
         for entry in entries:
             entry_count += 1
+
+            ID = entry["ID"].split("-", 2)
+            
+            venue = ID[0]
+            year = ID[1]
+            author = ID[2] if len(ID) == 3 else "n/a"
+            
             pdf_dst_path = dirname(bibfilepath) + sep + entry["ID"] + ".pdf"
-            if exists(pdf_dst_path):
-                pdf_count += 1
-                with open(log_file_path, "a") as logfile:
-                    logfile.write("   - " + entry["ID"] + ".pdf" + "\n")
+                
+            if "doi" in entry:
+                
+                pdf_src_path_wlgc = ir_anthology_root + "sources/wlgc/papers-by-doi/" + entry["doi"] + ".pdf"
+                
+                if exists(pdf_src_path_wlgc):
+                    wlgc = True
+                    if not exists(pdf_dst_path):
+                        if not DRY_RUN:
+                            copyfile(pdf_src_path_wlgc, pdf_dst_path)
+                else:
+                    wlgc = False
+
+                if True in [wlgc]:
+                    pdf_count += 1
+                    
+                with open(log_file_path, "a") as csv_file:
+                    csv_writer = writer(csv_file, delimiter=",")
+                    csv_writer.writerow([entry_count, venue, year, author, entry["doi"], "wlgc" if wlgc else "", ""])
             else:
-                if "doi" in entry:
-                    pdf_src_path = ir_anthology_root + "sources/wlgc/papers-by-doi/" + entry["doi"] + ".pdf"
-                    if exists(pdf_src_path):
-                        if not DRY_RUN: copyfile(pdf_src_path, pdf_dst_path)
-                        pdf_count += 1
-                        with open(log_file_path, "a") as logfile:
-                            logfile.write("   - " + entry["ID"] + ".pdf" + "\n")
+                with open(log_file_path, "a") as csv_file:
+                    csv_writer = writer(csv_file, delimiter=",")
+                    csv_writer.writerow([entry_count, venue, year, author, "", "", ""])
                         
-with open(log_file_path, "a") as logfile:
+with open("copy_pdfs_" + start + ".txt", "w") as logfile:
     logfile.write("\nPDFs: " + str(pdf_count) + "\n")
     logfile.write("PDFs: " + str(entry_count) + "\n")
