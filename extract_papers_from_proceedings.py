90c340ab7a244be132792721eac69ac60c70c19c
Wolfgang Kircheis <wolfgang.kircheis@uni-leipzig.de>
2023-11-01 18:09:23 +0100

    Add function to extract known papers

--- extract_papers_from_proceedings.py
+++ extract_papers_from_proceedings.py
@@ -1,12 +1,14 @@
 import fitz
-from os.path import exists
+import bibtexparser
+from glob import glob
+from os.path import dirname, exists, sep
 from os import makedirs
 from re import findall, search
 from pprint import pprint
 
 doi_regex = "10\.\d{4,9}/[-\._;\(\)/:a-zA-Z0-9]+"
 
-def run(pdfpath, contentstart, contentend, documentend, offset = 0):
+def extract_with_contents(pdfpath, contentstart, contentend, documentend, offset = 0):
 
     with fitz.open(pdfpath) as pdf:
 
@@ -49,7 +51,7 @@ def run(pdfpath, contentstart, contentend, documentend, offset = 0):
                 makedirs(prefix)
             paper.save(papers[index][2] + ".pdf")
 
-def run_no_contents(pdfpath, contentstart, contentend, documentend, offset):
+def extract_without_contents(pdfpath, contentstart, contentend, documentend, offset):
 
     with fitz.open(pdfpath) as pdf:
 
@@ -71,6 +73,35 @@ def run_no_contents(pdfpath, contentstart, contentend, documentend, offset):
         for paper in papers:
             print(paper)
 
+def extract_known(directory, proceedings_pdfs, paper_bib):
+    with open(paper_bib) as file:
+        entries = [(entry["ID"], entry["title"], entry.get("doi", None), entry.get("pages")) for entry in bibtexparser.load(file).entries]
+
+    entries = [(bibkey, title, doi, int(pages.split("--")[1]) - int(pages.split("--")[0])) for bibkey, title, doi, pages in entries if doi and pages]
+
+    entries_found = []
+
+    for proceedings_pdf in proceedings_pdfs:
+        with fitz.open(proceedings_pdf) as pdf:
+            for page_number, page in enumerate(pdf.pages()):
+                page_text = page.get_text().replace("\n", " ")
+                for bibkey, title, doi, pages in entries:
+                    doi = doi.replace("\\", "")
+                    if doi in page_text and title in page_text:
+                        entries_found.append([bibkey, title, doi, proceedings_pdf, page_number, page_number+pages])
+
+    for bibkey, title, doi, proceedings_pdf, from_page, to_page in entries_found:
+        with fitz.open(proceedings_pdf) as pdf:
+            paper = fitz.open()
+            paper.insert_pdf(pdf, from_page=from_page, to_page=to_page)
+            filepath = directory + "/" + doi + ".pdf"
+            if not exists(dirname(filepath)):
+                makedirs(dirname(filepath))
+            paper.save(filepath)
+
+    return len(entries_found)
+
+
 if __name__ == "__main__":
 
     #run("chiir23-proceedings.pdf", 7, 12, 519, 20)
@@ -79,5 +110,16 @@ if __name__ == "__main__":
     #run("ecir23-proceedings-part-2.pdf", 22, 27, 727)
     #run_no_contents("ecir23-proceedings-part-2.pdf", 22, 27, 727)
 
-    run_no_contents("adcs22-proceedings.pdf", 7, 12, 48, 20)
-    
+    #run_no_contents("adcs22-proceedings.pdf", 7, 12, 48, 20)
+
+    directories = glob("resources/*/*")
+
+    paper_count = 0
+
+    for directory in directories:
+        root, venue, year = directory.split("/")    
+        paper_count += extract_known(directory,
+                                     glob(directory + sep + "*-proceedings*.pdf"),
+                                     glob(directory + sep + "*.bib")[0])
+
+    print(paper_count)
